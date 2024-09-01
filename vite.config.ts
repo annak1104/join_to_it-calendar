ceadbed3f4d5da79818f5e4c574b27abcb35059e
annak1104 <andreynakonechnuy11@gmail.com>
2024-09-01 20:49:42 +0300

    deploy settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/join_to_it-calendar/',
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', '@chakra-ui/react', '@emotion/react', '@emotion/styled'],
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000,
+  },
+});
