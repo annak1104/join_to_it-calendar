3b76a5eea97c5157971621cf221cf66a6d15ebe0
annak1104 <andreynakonechnuy11@gmail.com>
2024-09-01 22:57:56 +0300

    feat: save event to local storage jc-4

--- src/Components/CalendarView/CalendarView.tsx
+++ src/Components/CalendarView/CalendarView.tsx
@@ -3,17 +3,22 @@ import { useState } from 'react';
 import { Calendar } from '../Calendar/Calendar';
 import { Appointment } from '../../types';
 import EventForm from '../EventForm/EventForm';
+import useLocalStorage from '../../hooks/useLocalStorage';
 
 export default function CalendarView() {
-  const [appointments, setAppointments] = useState<Appointment[]>([]);
+  const [appointments, setAppointments] = useLocalStorage<Appointment[]>(
+      'appointments',
+      []
+  );
   const [selectedAppointment, setSelectedAppointment] =
-    useState<Appointment | null>(null);
+      useState<Appointment | null>(null);
 
   const handleSubmit = (appointment: Appointment) => {
     if (appointment.id) {
-      setAppointments(prevAppointments =>
-        prevAppointments.map(a => (a.id === appointment.id ? appointment : a)),
+      const updatedAppointments = appointments.map(a =>
+          a.id === appointment.id ? appointment : a
       );
+      setAppointments(updatedAppointments);
     } else {
       const newAppointment = {
         ...appointment,
@@ -25,46 +30,33 @@ export default function CalendarView() {
   };
 
   const handleDelete = (id: string) => {
-    setAppointments(prevAppointments =>
-      prevAppointments.filter(a => a.id !== id),
-    );
+    const filteredAppointments = appointments.filter(a => a.id !== id);
+    setAppointments(filteredAppointments);
     setSelectedAppointment(null);
   };
 
-  const handleEventDelete = (appointmentId: string) => {
-    handleDelete(appointmentId);
-  };
-
-  const handleUpdateAppointment = (updatedAppointment: Appointment) => {
-    setAppointments(prevAppointments =>
-      prevAppointments.map(a =>
-        a.id === updatedAppointment.id ? updatedAppointment : a,
-      ),
-    );
-  };
-
   return (
-    <Flex gap={10} m={4} justifyContent={'center'} height="100%">
-      <Flex grow={1} flexBasis={'100%'} overflow="auto" position={'relative'}>
-        <Calendar
-          appointments={appointments}
-          onShowAppointmentView={appointment =>
-            setSelectedAppointment(appointment)
-          }
-          onDeleteAppointment={handleEventDelete}
-          onUpdateAppointment={handleUpdateAppointment}
-        />
-      </Flex>
-      <Flex width={'25%'} position={'absolute'} zIndex={'5'}>
-        {selectedAppointment && (
-          <EventForm
-            appointment={selectedAppointment}
-            onSubmit={handleSubmit}
-            onDelete={handleDelete}
-            onClose={() => setSelectedAppointment(null)}
+      <Flex gap={10} m={4} justifyContent={'center'} height="100%">
+        <Flex grow={1} flexBasis={'100%'} overflow="auto" position={'relative'}>
+          <Calendar
+              appointments={appointments}
+              onShowAppointmentView={appointment =>
+                  setSelectedAppointment(appointment)
+              }
+              onDeleteAppointment={handleDelete}
+              onUpdateAppointment={handleSubmit}
           />
-        )}
+        </Flex>
+        <Flex width={'25%'} position={'absolute'} zIndex={'5'}>
+          {selectedAppointment && (
+              <EventForm
+                  appointment={selectedAppointment}
+                  onSubmit={handleSubmit}
+                  onDelete={handleDelete}
+                  onClose={() => setSelectedAppointment(null)}
+              />
+          )}
+        </Flex>
       </Flex>
-    </Flex>
   );
 }
